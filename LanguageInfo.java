5b6187637f5924c4d91e0776b0d55abcab774e80
SureshGithubWorks <124015641+SureshGithubWorks@users.noreply.github.com>
2023-01-31 08:32:45 +0530

    Add files via upload

---
+++ LanguageInfo.java
@@ -0,0 +1,31 @@
+package org.lang;
+
+public class LanguageInfo {
+	
+	private void tamilLanguage() {
+		System.out.println("tamilnadu mother tang");
+		
+	}
+	private void englishLanguage() {
+		System.out.println("all Country General Language");
+
+	}
+	private void hindhiLanguage() {
+		System.out.println("indian mother tang");
+
+	}
+	public static void main(String[] args) {
+	    //class name object = new class name;
+		LanguageInfo n =new LanguageInfo();
+	n.tamilLanguage();
+	n.englishLanguage();
+	n.hindhiLanguage();
+	
+
+		
+	}
+		
+	}
+	
+
+
---
+++ StateDetails.java
@@ -0,0 +1,19 @@
+package org.lang;
+
+public class StateDetails {
+	private void southindia() {
+		System.out.println("tamilnadu");
+
+	}
+		private void northindia() {
+			System.out.println("northindia");
+			
+	}
+	public static void main(String[] args) {
+		//class name object = new class name;
+		StateDetails n = new StateDetails();
+		n.southindia();
+		n.northindia();
+	}
+
+}
